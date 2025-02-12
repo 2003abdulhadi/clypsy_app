f9935dddc71d18e2f77848273b60910cd23e1d06
Abdul Hadi <2003abdulhadi@gmail.com>
2025-02-11 22:25:20 -0500

    Format to 4 spaces

--- Common/Globals.cs
+++ Common/Globals.cs
@@ -2,9 +2,9 @@ using System;
 
 namespace clypsy.Common
 {
-  public static class Globals
-  {
-    public static readonly string apiBaseUrl = Environment.GetEnvironmentVariable("API_BASE_URL") ?? throw new InvalidOperationException("API_BASE_URL is not set in the environment.");
-    public static readonly string projectName = Environment.GetEnvironmentVariable("PROJECT_NAME") ?? throw new InvalidOperationException("PROJECT_NAME is not set in the environment.");
-  }
+    public static class Globals
+    {
+        public static readonly string apiBaseUrl = Environment.GetEnvironmentVariable("API_BASE_URL") ?? throw new InvalidOperationException("API_BASE_URL is not set in the environment.");
+        public static readonly string projectName = Environment.GetEnvironmentVariable("PROJECT_NAME") ?? throw new InvalidOperationException("PROJECT_NAME is not set in the environment.");
+    }
 }
